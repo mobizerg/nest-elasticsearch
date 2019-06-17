bed39a513d0a260cbb045489d13e72a64bda6625
Frozenex <arun.frozenex@gmail.com>
2019-06-17 17:35:00 +0530

    Removed lodash

--- lib/elasticsearch.helper.ts
+++ lib/elasticsearch.helper.ts
@@ -1,5 +1,3 @@
-import { upperCase } from 'lodash';
-
 export function getClientToken(name?: string): string {
-  return name ? `ELASTICSEARCH_CLIENT_${upperCase(name)}` : 'ELASTICSEARCH_CLIENT_DEFAULT';
+  return name ? `ELASTICSEARCH_CLIENT_${name.toUpperCase()}` : 'ELASTICSEARCH_CLIENT_DEFAULT';
 }
